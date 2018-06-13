206b03ebd404a502a2ce56f30725a5175b5e2b27
Élisson Michael <elissonmichael@yahoo.com.br>
2018-06-13 18:20:17 -0300

    Separa os Métodos em Main

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -3,16 +3,25 @@ import dao.PessoaDAO;
 import modelo.Pessoa;
 
 public class Principal {
-	
-	public static void exemploDeUso() {
+	public static void exemploDeCriacao() {
 		Pessoa fulano = new Pessoa("Fulano");
-		fulano.DAO().salvar();
-		Pessoa sicrano = new Pessoa("Sicrano");
-		sicrano.DAO().salvar();
-		for(Pessoa pessoa : PessoaDAO.listar()) {
-			System.out.println(pessoa);
+		fulano.DAO().salvar(); 
+		new Pessoa("Sicrano").DAO().salvar();
+	}
+
+	public static void exemploDePesquisaComFiltro() {
+		for(Pessoa pessoa : PessoaDAO.filtrar("nome", "Sicrano"))
+			System.out.println("Filtro: " + pessoa);
+	}
+	
+	public static void exemploDeListagemCompleta() {
+		for(Pessoa pessoa : PessoaDAO.listar())
+			System.out.println(pessoa);	
+	}
+	
+	public static void exemploDeExclusao() {
+		for(Pessoa pessoa : PessoaDAO.listar())
 			pessoa.DAO().excluir();
-		}
 	}
 	
 	public static void main(String[] args) {
